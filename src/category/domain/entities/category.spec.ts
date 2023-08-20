b479afda0136a3ec81b5facea7c623d26c5c9ab2
FabioRocha231 <fabioharoldo1@gmail.com>
2023-08-20 00:05:41 -0300

    test: category constructor

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,20 +1,48 @@
 import { Category } from './category'
-
+import { omit } from 'lodash'
 describe('Category Unity Tests', () => {
   test('constructor of category', () => {
-    const mock_created_at = new Date()
-    const category = new Category({
+    let category = new Category({
+      name: 'Movie',
+    })
+    const props = omit(category.props, ['created_at'])
+    expect(props).toStrictEqual({
       name: 'Movie',
       is_active: true,
-      description: 'Movie description',
-      created_at: mock_created_at,
+      description: null,
     })
+    expect(category.props.created_at).toBeInstanceOf(Date)
 
+    category = new Category({
+      name: 'Movie',
+      description: 'Movie description',
+      is_active: false,
+    })
+    let created_at = new Date()
     expect(category.props).toStrictEqual({
       name: 'Movie',
-      is_active: true,
       description: 'Movie description',
-      created_at: mock_created_at,
+      is_active: false,
+      created_at,
+    })
+
+    category = new Category({
+      name: 'Movie',
+      is_active: true,
+    })
+    expect(category.props).toMatchObject({
+      name: 'Movie',
+      is_active: true,
+    })
+
+    created_at = new Date()
+    category = new Category({
+      name: 'Movie',
+      created_at,
+    })
+    expect(category.props).toMatchObject({
+      name: 'Movie',
+      created_at,
     })
   })
 
